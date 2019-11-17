fb14d4eeb10fde31a041fa91352ed1a3fa6f9011
William Taylor <willtaylor2@hotmail.com>
2019-11-17 15:02:52 +0000

    added sagas

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,15 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './modules/reducer'
-import { forbiddenWordsMiddleware } from './middleware'
-import thunk from 'redux-thunk'
+// import { forbiddenWordsMiddleware } from './middleware'
+import createSagaMiddleware from 'redux-saga'
+import { watcherSaga } from './modules/articles'
 
+const initialiseSagaMiddleware = createSagaMiddleware()
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
 const store = createStore(
   rootReducer,
   storeEnhancers(
-    applyMiddleware(thunk, forbiddenWordsMiddleware)
+    applyMiddleware(initialiseSagaMiddleware)
   )
 )
+initialiseSagaMiddleware.run(watcherSaga)
 
 export default store
--- src/redux/modules/articles.js
+++ src/redux/modules/articles.js
@@ -1,6 +1,34 @@
-// constants
+import { takeEvery, call, put } from 'redux-saga/effects'
+
+// Action constants
 export const ADD_ARTICLE = 'ADD_ARTICLE'
 export const DATA_LOADED = 'DATA_LOADED'
+export const DATA_REQUESTED = 'DATA_REQUESTED'
+export const API_ERRORED = 'API_ERRORED'
+
+// Action creators
+export const addArticle = (payload) => ({ type: ADD_ARTICLE, payload })
+export const getData = () => ({ type: DATA_REQUESTED })
+
+// Sagas
+export function * watcherSaga () {
+  yield takeEvery(DATA_REQUESTED, workerSaga)
+}
+
+function * workerSaga () {
+  try {
+    const payload = yield call(getSagaData)
+    yield put({ type: DATA_LOADED, payload })
+  } catch (e) {
+    yield put({ type: API_ERRORED, payload: e })
+  }
+}
+
+function getSagaData () {
+  return fetch('https://jsonplaceholder.typicode.com/posts').then(response =>
+    response.json()
+  )
+}
 
 // reducers
 const initialState = {
@@ -8,32 +36,21 @@ const initialState = {
   remoteArticles: []
 }
 
-export default function reducer (state = initialState, action) {
-  console.log(state.remoteArticles)
-  if (action.type === ADD_ARTICLE) {
-    return Object.assign({}, state, {
-      articles: state.articles.concat(action.payload)
-    })
-  }
-  if (action.type === DATA_LOADED) {
-    return Object.assign({}, state, {
-      remoteArticles: state.remoteArticles.concat(action.payload)
-    })
-  }
-  return state
-}
-
-// actions
-export function addArticle (payload) {
-  return { type: ADD_ARTICLE, payload }
-}
-
-export function getData () {
-  return function (dispatch) {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(json => {
-        dispatch({ type: DATA_LOADED, payload: json })
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE: {
+      return Object.assign({}, state, {
+        articles: state.articles.concat(action.payload)
       })
+    }
+    case DATA_LOADED: {
+      return Object.assign({}, state, {
+        remoteArticles: state.remoteArticles.concat(action.payload)
+      })
+    }
+    default:
+      return state
   }
 }
+
+export default reducer
